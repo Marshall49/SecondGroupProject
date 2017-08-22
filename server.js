a0a0aa3933cf760be38f168957f0ccc15a085c95
Malcolm Strong <strong.malcolm@yahoo.com>
2017-08-21 21:04:05 -0400

    added the index.html links and a placeholder photo

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
+var methodOverride = require("method-override");
+
 
 
 var app = express();
 var PORT = process.env.PORT || 8080;
+
+app.use(methodOverride("_method"));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
