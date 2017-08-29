4131843009b21682ed9920fce533b282f3fc4c05
Safiyya Sharif <SafiyyaSharif@lawn-128-61-6-242.lawn.gatech.edu>
2017-08-29 19:33:33 -0400

    updated schema and seeds

--- DB/schema.sql
+++ DB/schema.sql
@@ -3,38 +3,42 @@ DROP DATABASE IF EXISTS fitbot_db;
 -- creating new database for fitness app
 CREATE DATABASE fitbot_db;
 -- creating table called app_users using the fitbot_db database
--- USE fitbot_db;
--- CREATE TABLE app_userInfo(
--- 	id INTEGER(11) AUTO_INCREMENT NOT NULL,
--- 	first_name VARCHAR(50) NOT NULL,
--- 	last_name VARCHAR(50) NOT NULL,
--- 	user_name VARCHAR(50) NOT NULL,
--- 	city VARCHAR(50) NOT NULL,
--- 	state VARCHAR(2) NOT NULL,
--- 	zip_code INTEGER(5) NOT NULL,
--- 	height_feet INTEGER(2) NOT NULL,
--- 	height_inches INTEGER(2) NOT NULL,
--- 	current_weight INTEGER(3) NOT NULL,
--- 	PRIMARY KEY (id)
--- );
+USE fitbot_db;
+CREATE TABLE app_userInfo(
+	id INTEGER(11) AUTO_INCREMENT NOT NULL,
+	first_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(50) NOT NULL,
+	user_name VARCHAR(50) NOT NULL,
+	city VARCHAR(50) NOT NULL,
+	state VARCHAR(2) NOT NULL,
+	zip_code INTEGER(5) NOT NULL,
+	height_feet INTEGER(2) NOT NULL,
+	height_inches INTEGER(2) NOT NULL,
+	current_weight INTEGER(3) NOT NULL,
+	PRIMARY KEY (id)
+);
 
--- USE fitbot_db;
--- CREATE TABLE user_weight(
--- 	week INTEGER(11) AUTO_INCREMENT NOT NULL,
--- 	current_weight INTEGER(3) NOT NULL,
--- 	PRIMARY KEY (week)
--- );
+USE fitbot_db;
+CREATE TABLE user_weight(
+	week INTEGER(11) AUTO_INCREMENT NOT NULL,
+	user_name VARCHAR(50) NOT NULL,
+	current_weight INTEGER(3) NOT NULL,
+	PRIMARY KEY (week)
+);
 
--- USE fitbot_db;
--- CREATE TABLE user_caloriesBurned(
--- 	week INTEGER(11) AUTO_INCREMENT NOT NULL,
--- 	calories_burned INTEGER(3) NOT NULL,
--- 	PRIMARY KEY (week)
--- );
+USE fitbot_db;
+CREATE TABLE user_caloriesBurned(
+	week INTEGER(11) AUTO_INCREMENT NOT NULL,
+	user_name VARCHAR(50) NOT NULL,
+	calories_burned INTEGER(3) NOT NULL,
+	PRIMARY KEY (week)
+);
 
--- USE fitbot_db;
--- CREATE TABLE user_mileage(
--- 	week INTEGER(11) AUTO_INCREMENT NOT NULL,
--- 	calories_burned INTEGER(3) NOT NULL,
--- 	PRIMARY KEY (id)
--- );
+USE fitbot_db;
+CREATE TABLE user_mileage(
+	week INTEGER(11) AUTO_INCREMENT NOT NULL,
+	user_name VARCHAR(50) NOT NULL,
+	total_mileage INTEGER(3) NOT NULL,
+	PRIMARY KEY (week)
+);
+
--- DB/seeds.sql
+++ DB/seeds.sql
@@ -1,4 +1,40 @@
 -- creating new rows of user info
-INSERT INTO app_users (first_name, last_name, city, state,
-zip_code, height_feet, height_inches, current_weight )
-VALUES ("John", "Davis", "Phoenix", "AZ", 85001, 5, 10, 163);
+INSERT INTO app_userInfo (first_name, last_name, user_name, city, state,
+zip_code, height_feet, height_inches, current_weight)
+VALUES ("John", "Davis", "JDavis", "Phoenix", "AZ", 85001, 5, 10, 163);
+
+INSERT INTO user_caloriesBurned (user_name, calories_burned)
+VALUES ("JDavis", 1500);
+INSERT INTO user_caloriesBurned (user_name, calories_burned)
+VALUES ("JDavis", 2000);
+INSERT INTO user_caloriesBurned (user_name, calories_burned)
+VALUES ("JDavis", 300);
+INSERT INTO user_caloriesBurned (user_name, calories_burned)
+VALUES ("JDavis", 2000);
+INSERT INTO user_caloriesBurned (user_name, calories_burned)
+VALUES ("JDavis", 1000);
+
+INSERT INTO user_mileage (user_name, total_mileage)
+VALUES ("JDavis", 5);
+INSERT INTO user_mileage (user_name, total_mileage)
+VALUES ("JDavis", 15);
+INSERT INTO user_mileage (user_name, total_mileage)
+VALUES ("JDavis", 20);
+INSERT INTO user_mileage (user_name, total_mileage)
+VALUES ("JDavis", 11);
+INSERT INTO user_mileage (user_name, total_mileage)
+VALUES ("JDavis", 28);
+
+INSERT INTO user_weight (user_name, current_weight)
+VALUES ("JDavis", 163);
+INSERT INTO user_weight (user_name, current_weight)
+VALUES ("JDavis", 163);
+INSERT INTO user_weight (user_name, current_weight)
+VALUES ("JDavis", 158);
+INSERT INTO user_weight (user_name, current_weight)
+VALUES ("JDavis", 153);
+INSERT INTO user_weight (user_name, current_weight)
+VALUES ("JDavis", 150);
+
+
+
