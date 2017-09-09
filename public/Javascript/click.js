8071b981131996cba21dada6ce5a3d43543664d0
Malcolm Strong <strong.malcolm@yahoo.com>
2017-09-09 15:08:57 -0400

    changed some stuff

--- public/Javascript/click.js
+++ public/Javascript/click.js
@@ -5,8 +5,14 @@ $(".button").on('click', function(){
 });
 
 $(".btn").on('click', function(){
-    $(".team").removeClass("hidden");//jQuery function which triggers when developers link clicked
+    //jQuery function which triggers when developers link clicked
     $(".team").css("display", "block");		//displays the previously hidden container class block
     lastElementTop = $('.team').position().top ; //finds the position of the container class and assigns to variable
     $('html, body').animate({scrollTop: lastElementTop}, 'slow'); //scrolls to the position found in last line
+});
+
+$(".js-scroll-trigger").click(function(){
+        // $("#div1").fadeIn();
+        // $("#div2").fadeIn("slow");
+        $(".social-btns").fadeIn(7000);
 });
